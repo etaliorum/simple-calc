633d93ed540047755401389d7bad51f02836f938
etaliorum <evgen.fw@gmail.com>
2020-01-21 15:47:22 +0200

    added main.py

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-if __name__ == '__main__':
-    print('test')
+a = int(input())
+b = int(input())
+operand = input()
+
+if (operand == '+'):
+    print(a + b)
+if (operand == '-'):
+    print(a - b)
+if (operand == '/'):
+    if b == 0:
+        print('Деление на 0!')
+    else:
+        print(a / b)
+if (operand == '*'):
+    print(a * b)
+if (operand == 'mod'):
+    if b == 0:
+        print('Деление на 0!')
+    else:
+        print(a % b)
+if operand == 'pow':
+    print(a ** b)
+if operand == 'div':
+    if b == 0:
+        print('Деление на 0!')
+    else:
+        print(a // b)
